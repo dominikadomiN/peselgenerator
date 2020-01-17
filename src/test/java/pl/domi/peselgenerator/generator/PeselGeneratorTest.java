f0f48950e168064f7dff7e57c86e0da2eb7da020
domiN <dominikasuzuki.polonia@gmail.com>
2020-01-17 13:12:03 +0100

    Refactoring PeselGeneratorTest to parameterized unit test

--- src/test/java/pl/domi/peselgenerator/generator/PeselGeneratorTest.java
+++ src/test/java/pl/domi/peselgenerator/generator/PeselGeneratorTest.java
@@ -1,6 +1,9 @@
 package pl.domi.peselgenerator.generator;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import pl.domi.peselgenerator.generator.exception.PeselGeneratorException;
 import pl.domi.peselgenerator.model.Gender;
 
@@ -9,55 +12,35 @@ import java.time.LocalDate;
 import static java.lang.Integer.parseInt;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(JUnitParamsRunner.class)
 public class PeselGeneratorTest {
 
     private PeselGenerator peselGenerator = new PeselGenerator();
 
-    @Test
-    public void shouldGeneratePeselForWomanWith1987() throws PeselGeneratorException {
-        //given
-        LocalDate dateOfBirth = LocalDate.of(1987, 3, 7);
-
-        // when
-        String actual = peselGenerator.generate(dateOfBirth, Gender.WOMAN);
-        int actualGenderModule = parseInt(actual.substring(9, 10)) % 2;
-
-        //then
-        assertEquals("87", actual.substring(0, 2));
-        assertEquals("03", actual.substring(2, 4));
-        assertEquals("07", actual.substring(4, 6));
-        assertEquals(0, actualGenderModule);
+    private Object[][] parametersForShouldGeneratePesel() {
+        return new Object[][]{
+                {LocalDate.of(1987, 3, 7), Gender.WOMAN, "87", "03", "07", 0},
+                {LocalDate.of(1875, 12, 24), Gender.MAN, "75", "92", "24", 1},
+                {LocalDate.of(2012, 4, 14), Gender.MAN, "12", "24", "14", 1}
+        };
     }
 
     @Test
-    public void shouldGeneratePeselForManWith1875() throws PeselGeneratorException {
-        //given
-        LocalDate dateOfBirth = LocalDate.of(1875, 12, 24);
-
-        // when
-        String actual = peselGenerator.generate(dateOfBirth, Gender.MAN);
+    @Parameters (method = "parametersForShouldGeneratePesel")
+    public void shouldGeneratePesel(LocalDate dateOfBirth,
+                                    Gender gender,
+                                    String expectedYear,
+                                    String expectedMonth,
+                                    String expectedDay,
+                                    int expectedGenderModule) throws PeselGeneratorException {
+        // given,when
+        String actual = peselGenerator.generate(dateOfBirth, gender);
         int actualGenderModule = parseInt(actual.substring(9, 10)) % 2;
 
         //then
-        assertEquals("75", actual.substring(0, 2));
-        assertEquals("92", actual.substring(2, 4));
-        assertEquals("24", actual.substring(4, 6));
-        assertEquals(1, actualGenderModule);
-    }
-
-    @Test
-    public void shouldGeneratePeselForManWith2012() throws PeselGeneratorException {
-        //given
-        LocalDate dateOfBirth = LocalDate.of(2012, 4, 14);
-
-        // when
-        String actual = peselGenerator.generate(dateOfBirth, Gender.MAN);
-        int actualGenderModule = parseInt(actual.substring(9, 10)) % 2;
-
-        //then
-        assertEquals("12", actual.substring(0, 2));
-        assertEquals("24", actual.substring(2, 4));
-        assertEquals("14", actual.substring(4, 6));
-        assertEquals(1, actualGenderModule);
+        assertEquals(expectedYear, actual.substring(0, 2));
+        assertEquals(expectedMonth, actual.substring(2, 4));
+        assertEquals(expectedDay, actual.substring(4, 6));
+        assertEquals(expectedGenderModule, actualGenderModule);
     }
 }
